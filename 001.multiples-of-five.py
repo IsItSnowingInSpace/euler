11bca4e4ca522e3402f82b9d30a679bb97d9cbc6
rickmckay <rickmckay@gmail.com>
2014-05-12 18:58:29 -0600

    First projecteuler.net exercise: tallies all multiples of 3 or 5 in seq 1-1000.

---
+++ 001.multiples-of-five.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+count = 1
+sum = 0
+
+while ( count < 1000 ):
+    if(( count % 3 == 0 ) or (count % 5 == 0)):
+        sum += count
+    count += 1
+
+print "sum = " + str(sum)
